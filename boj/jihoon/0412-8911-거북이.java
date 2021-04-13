052dca5e000d19a1bf99d6c34f64e7cf22e06e35
hoonti06 <hoonti06@gmail.com>
2021-04-13 20:10:26 +0900

    Update 0412-8911-거북이.java

--- boj/jihoon/0412-8911-거북이.java
+++ boj/jihoon/0412-8911-거북이.java
@@ -1,18 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Main {
-    static final int MAX_N = 1010;
-    
     static int N;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = { 0, 1, 0,-1};
-    static int[][] board = new int[MAX_N][MAX_N];
     
     static int solution(char[] orders) {
         int dir = 0;
-        int r = 505, c = 505;
+        int r = 0, c = 0;
         int maxR, maxC, minR, minC;
-        maxR = maxC = minR = minC = 505;
+        maxR = maxC = minR = minC = 0;
         for (int i = 0; i < orders.length; i++) {
             char order = orders[i];
             if (order == 'L')
@@ -22,17 +21,15 @@ public class Main {
             else if (order == 'F') {
                 r += dx[dir];
                 c += dy[dir];
-                
                 maxR = Math.max(maxR, r);
                 minR = Math.min(minR, r);
                 maxC = Math.max(maxC, c);
                 minC = Math.min(minC, c);
             }
             else {
-                int backDir = (dir + 2) %4;
+                int backDir = (dir + 2) % 4;
                 r += dx[backDir];
                 c += dy[backDir];
-                
                 maxR = Math.max(maxR, r);
                 minR = Math.min(minR, r);
                 maxC = Math.max(maxC, c);
@@ -44,7 +41,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(in.readLine());
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             System.out.println(solution(in.readLine().toCharArray()));
+        }
     }
 }
